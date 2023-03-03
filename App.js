31355399c11ddb846ef3a1699debc3461242260c
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-03-03 16:27:03 -0500

    inline styling

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-
 import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View>
+        <Text>Another piece of text!</Text>
+      </View>
       <Text>Hello World!</Text>
-      
+      <Button title="Tap me!" />
     </View>
   );
 }
@@ -18,4 +20,3 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
-
